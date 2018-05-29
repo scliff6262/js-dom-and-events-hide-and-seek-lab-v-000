f778d241734bd932d6bd65d2b269161b4bf6fd46
Sean Clifford <sclifford6262@gmail.com>
2018-05-29 14:11:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ function increaseRankBy(n){
 
 function deepestChild(){
   let grandNode = document.querySelector('#grand-node')
-  
+
   function goDeeper(){
     if (grandNode.querySelector('div')){
       grandNode = grandNode.querySelector('div')
       goDeeper()
     } else {
-      return grandNode
+      return grandNode.innerHTML
     }
   }
-  
+
 }
