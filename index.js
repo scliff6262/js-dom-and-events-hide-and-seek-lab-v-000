5fada03b525247bdf3e038dfb8822ac1a8f9d14e
Sean Clifford <sclifford6262@gmail.com>
2018-05-29 14:02:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  const listItems = document.querySelectorAll('.ranked-list li')
+  for (let i = 0; i < listItems.length; i++){
+    listItems[i].innerHTML = parseInt(listItems[i].innerHTML) + n
+  }
+}
